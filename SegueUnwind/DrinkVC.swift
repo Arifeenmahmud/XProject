7c164bf3c97f932cf34a3b806ccfd6b53e055a9e
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2019-12-24 03:27:22 +0600

    Initial Commit

---
+++ SegueUnwind/DrinkVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SegueUnwind
+//
+//  Created by Arifeen Mahmud on 22/12/19.
+//  Copyright © 2019 Arifeen Mahmud. All rights reserved.
+//
+
+import UIKit
+class DrinkVC: UIViewController {
+    @IBOutlet weak var drinkDisplayLabel: UILabel!
+    @IBOutlet weak var foodTextFild: UITextField!
+    
+    var DrinkValue = String!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        drinkDisplayLabel.text = drinkValue
+
+    }
+    
+}
---
+++ SegueUnwind/foodVC.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  SegueUnwind
+//
+//  Created by Arifeen Mahmud on 22/12/19.
+//  Copyright © 2019 Arifeen Mahmud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.destination is DrinkVC else {return}
+        
+    }
+
+}
