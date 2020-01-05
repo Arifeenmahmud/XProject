4eb4dc92ab00697cb603c7f8a6b87f79c3fef871
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2020-01-05 22:21:03 +0600

    Initial Commit

---
+++ PlayerInformation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayerInformation
+//
+//  Created by Arifeen Mahmud on 5/1/20.
+//  Copyright © 2020 Arifeen Mahmud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
