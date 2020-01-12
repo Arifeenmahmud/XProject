959da9c05bd440d3a613f5f86d72c464d40f9f7d
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2020-01-12 19:26:29 +0600

    Table View Data Show

--- Mullitable/ViewController.swift
+++ Mullitable/ViewController.swift
@@ -8,13 +8,95 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var table1: UITableView!
+    @IBOutlet var table2: UITableView!
+    
+    
+    
+    let teams : [String] = ["Arsenal","Chelsea","Manchester"]
+    
+    let ArsenalPlayer : [String] = ["Arsenal","Arsenal","Arsenal","Arsenal","Arsenal"]
+    let ChelseaPlayer : [String] = ["Chelsea","Chelsea","Chelsea","Chelsea","Chelsea"]
+    let ManchesterPlayer : [String] = ["Manchester","Manchester","Manchester","Manchester"]
+    
+    var teamReference : String = "Arsenal"
+    let cellIdentifier : String = "Cell"
+    
+    var numberOfTeams : Int = 0
+    var numberOfPlayers : Int = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table1.delegate = self
+        table2.delegate = self
+        
+        table1.dataSource = self
+        table2.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (tableView.tag == 1)
+        {
+            numberOfTeams = teams.count
+            return numberOfTeams
+            
+        }
+        else if (tableView.tag == 2)
+        {
+            if (teamReference == "Arsenal")
+            {
+                numberOfPlayers = ArsenalPlayer.count
+            }
+            else if (teamReference == "Chelsea")
+            {
+                numberOfPlayers = ChelseaPlayer.count
+            }
+            else if (teamReference == "Manchester")
+            {
+                numberOfPlayers = ManchesterPlayer.count
+            }
+            return numberOfPlayers
+        }
+        else
+        {
+            return 0
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as UITableViewCell
+        
+        if (tableView.tag == 1)
+        {
+            Cell.textLabel?.text = teams[indexPath.row]
+        }
+        else if (tableView.tag == 2)
+        {
+            if (teamReference == "Arsenal")
+            {
+                Cell.textLabel?.text = ArsenalPlayer[indexPath.row]
+            }
+            else if (teamReference == "Chelsea")
+            {
+                Cell.textLabel?.text = ChelseaPlayer[indexPath.row]
+            }
+            else if (teamReference == "Manchester")
+            {
+                Cell.textLabel?.text = ManchesterPlayer[indexPath.row]
+            }
+        }
+        return Cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (tableView.tag == 1)
+        {
+            teamReference = teams[indexPath.row]
+        }
+        table2.reloadData()
+    }
 
 }
 
