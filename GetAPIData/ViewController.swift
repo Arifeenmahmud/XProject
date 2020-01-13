91b35cb853dff0a7b94c32ad63098895a4c4de3d
arifeen <arifeen.mahmud@yahoo.com>
2020-01-13 15:50:47 +0600

    Initial Commit

---
+++ GetAPIData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetAPIData
+//
+//  Created by Lab on 13/1/20.
+//  Copyright © 2020 Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
