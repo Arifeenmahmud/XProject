e55eb50bcb37643a24ae4cb1a00715035379f70d
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2020-01-22 00:26:09 +0600

    Parsing Json

--- ParsingJSON/ViewController.swift
+++ ParsingJSON/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
+struct Country: Decodable {
+    let name: String
+    let capatil: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do{
+                let countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for country in countries {
+                    print(country.name + ":" + country.capatil)
+                }
+                
+            } catch{
+                print("We got an Error")
+            }
+        }.resume()
+        
     }
-
 
 }
 
