854e5c4264037614db7b4271e1c9926bd5ebf8f7
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2022-07-06 17:42:40 +0600

    Message Dialog

--- programming jokes/ViewController.swift
+++ programming jokes/ViewController.swift
@@ -13,7 +13,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func ClickForAction(_ sender: Any) {
+        let refreshAlert = UIAlertController(title: "Refresh", message: "All data will be lost.", preferredStyle: UIAlertController.Style.alert)
+
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+              print("Handle Ok logic here")
+        }))
+        
+        
+//        refreshAlert.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+//              print("Handle Ok logic here")
+//        }))
 
 
+        present(refreshAlert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func test(_ sender: Any) {
+        // create the alert
+              let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: .alert)
+
+              // add an action (button)
+              alert.addAction(UIAlertAction(title: "OkK", style: UIAlertAction.Style.default, handler: nil))
+
+              // show the alert
+              self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
